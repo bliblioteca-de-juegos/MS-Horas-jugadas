48957263196d80bc261951d66264704f7a31d2c2
Sajnovich <101999288+Sajnovich@users.noreply.github.com>
2026-05-01 16:36:15 -0400

    se agrega ms-horas-jugadas
    Se agrega configuración estándar, solo es el inicio del microservicio

---
+++ Ms_horas_jugadas/src/main/java/com/biblioteca/Ms_horas_jugadas/MsHorasJugadasApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.Ms_horas_jugadas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsHorasJugadasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsHorasJugadasApplication.class, args);
+	}
+
+}
---
+++ Ms_horas_jugadas/src/test/java/com/biblioteca/Ms_horas_jugadas/MsHorasJugadasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.Ms_horas_jugadas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsHorasJugadasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
