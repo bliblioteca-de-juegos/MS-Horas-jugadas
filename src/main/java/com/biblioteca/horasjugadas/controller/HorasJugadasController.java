e537cbadf20a804d99fd2cf342de06c48c876737
Sajnovich <eduardo.sajnovich.esc@gmail.com>
2026-06-20 21:03:06 -0400

    Se agrega Swagger, se agrega tambien la version de las apis, se documentan y versionan ednpoints

--- src/main/java/com/biblioteca/horasjugadas/controller/HorasJugadasController.java
+++ src/main/java/com/biblioteca/horasjugadas/controller/HorasJugadasController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/horas-jugadas")
+@RequestMapping("/api/v2/horas-jugadas")
 @RequiredArgsConstructor
 public class HorasJugadasController {
 
